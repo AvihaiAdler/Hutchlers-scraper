38ae8d0e9d3d3a81e74a0bfc424e0b3698b453fc
adler <avihai.adler@gmail.com>
2023-05-21 17:21:38 +0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+### A Simple URL scraper 
+a simple tool design to scrape a html (local) document and send all the links within it to some endpoint
+
+### Usage
+the scrapper expects some arguments upon launch. all arguments must be passed in the command line upon launchiing the script.
+the first argument is the endpoint to `POST` messages to. in the context of discord - that would be a webhook url. said url _must be kept as secret!_.
+creating a webhook endpoint is explained [here](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks)
+
+all following arguments are paths to a local html document one wants to scrape.
+
+an example might look like: `python url_scrapper.py https://discord.com/api/webhooks/secret path/to/html/document_1.html path/to/html/document_2.html`
+
+### Setting up
+- make sure you have python 3.8 installed
+- clone the repo
+- navigate to the project directory, open a terminal and run `python3 -m venv .venv`. this will create a virtual environment in the current directory (which may take a few seconds)
+- run `source .venv/bin/activate` to activate the virtual environment (`.venv\Scripts\activate.bat`). (`deactivate` in case you want to deactivate it)
+- within the virtual enviroment run `pip3 install -r requirements.txt`. this will install all the necessary modules to the virtual environment 
+- run `python url_scrapper.py` with all the necessary arguments
---
+++ url_scrapper.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+import sys
+import requests
+import time
+
+# POST_URL: str = "https://discord.com/api/webhooks/1094226591351066794/v9Imkmx2TunkxBLKAaDGaCn5WXFlr76ra7oNq72VeTuqpUP7-aeHlgSJg4HawP7ryZwY"
+CHARACTER_LIMIT: int = 2000
+MESSAGE_LIMIT: int = 10
+
+
+def extract_attacments_urls(html_doc: str, parser_type: str) -> set[str] | None:
+    """searches an html document for all links who aren't begin with a '#'
+
+    Args:
+        html_doc (str): a path to an html document
+        parser_type (str): a parser type for BeautifulSoap. should always be "html.parser" here
+
+    Returns:
+        set[str] | None: a set containing all unique urls or None if there are non
+    """
+    with open(html_doc, "r") as html:
+        soup_tree = BeautifulSoup(html, parser_type)
+        return set(
+            link.get("href")
+            for link in soup_tree.find_all("a")
+            if link.get("href")[0] != "#"
+        )
+
+
+def construct_content(url_list: set[str], char_limit: int) -> dict[int, str]:
+    """contruct message content from a list of urls.
+    each message must be below 2000 chars to satisfy Discord's limit
+
+    Args:
+        url_list (set[str]): a set containing all urls
+        char_limit (int): the character limit per message. 2000 as per the Discord API
+
+    Returns:
+        dict[int, str]: a dictionary with a message number as a key
+        and the message itself as the value
+    """
+    ret: dict[int, str] = {}
+
+    index: int = 0
+    current_len: int = 0
+    for url in url_list:
+        tmp_len = len(url)
+        if current_len + 1 + tmp_len < char_limit:
+            ret[index] = ret.get(index, url) + " " + url
+            current_len += tmp_len
+        else:
+            index += 1
+            current_len = 0
+    return ret
+
+
+def main() -> None:
+    # cmd arguments
+    if len(sys.argv) < 3:
+        print(
+            f"Usage: python <{sys.argv[0]}> <webhook_url> <path/to/html/file> <path/to/html/file> ..."
+        )
+        exit()
+
+    post_url: str = sys.argv[1]
+
+    for file_name in sys.argv[2:]:
+        urls: set[str] = extract_attacments_urls(file_name, "html.parser")
+        if urls is None:
+            continue
+
+        content: dict[int, str] = construct_content(urls, CHARACTER_LIMIT)
+        for key, message_content in content.items():
+            response: requests.Response = requests.post(
+                post_url,
+                data={"username": "scrapper-webhook", "content": message_content},
+            )
+
+            if 200 > response.status_code >= 300:
+                print(f"Error: {response.status_code}: {response.reason}")
+
+            # delay further messages to stay below Discord's (estimated) rate limit
+            if key and key % MESSAGE_LIMIT == 0:
+                time.sleep(0.5)
+
+
+main()
